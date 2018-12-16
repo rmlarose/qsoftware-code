3f5f4149fec68afa7a4e0dc0089cc21c7392f5eb
Ryan LaRose <rlarose@umich.edu>
2018-12-16 14:48:42 -0500

    Syntax updates to ProjectQ's random.py for ProjectQ v0.4.1.

--- projectq/random/random.py
+++ projectq/random/random.py
@@ -1,37 +1,36 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# ==========================================================
-# random.py
-#
-# Random number generator circuit in ProjectQ.
-#
-# written by Ryan LaRose <laroser1@msu.edu>
-# at Michigan State University 05-14-2018
-# ==========================================================
+"""Random number generator circuit in ProjectQ v0.4.1."""
 
-# ---------------------------------------------------------
+# =============================================================================
 # imports
-# ---------------------------------------------------------
+# =============================================================================
+
 from projectq import MainEngine
 import projectq.ops as ops
+from projectq.backends import CircuitDrawer
 
+# =============================================================================
+# imports
+# =============================================================================
+
+# grab an engine
 eng = MainEngine()
 
+# get some qubits
 qbits = eng.allocate_qureg(1)
 
+# add instructions to the circuit
 ops.H | qbits[0]
 ops.Measure | qbits[0]
 
+# run the circuit
 eng.flush()
 
+# display the result
 print(int(qbits[0]))
 
-
-# --------------------------------------------------------
-# optionally draw the circuit
-# --------------------------------------------------------
-
-from projectq.backends import CircuitDrawer
+# =============================================================================
+# generate tex code for drawing the circuit with tikz
+# =============================================================================
 
 drawing_engine = CircuitDrawer()
 print(drawing_engine.get_latex())
