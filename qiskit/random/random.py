7be09c64dc2159caa878c84e56e1adc6f8cba1c4
Ryan LaRose <rlarose@umich.edu>
2018-12-16 13:54:26 -0500

    Updated QISKit random.py for QISKit v0.6.1.

--- qiskit/random/random.py
+++ qiskit/random/random.py
@@ -1,53 +1,38 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+"""Random bit generator circuit in QISKit v0.6.1."""
 
-# ========================================================
-# random.py
-#
-# Random number generator circuit in QISKit.
-#
-# written by Ryan LaRose <laroser1@msu.edu>
-# at Michigan State University 05-14-18
-# ========================================================
-
-# --------------------------------------------------------
+# =============================================================================
 # imports
-# --------------------------------------------------------
+# =============================================================================
 
-from qiskit import QuantumCircuit
+from qiskit import (Aer, 
+                    ClassicalRegister,
+                    execute,
+                    QuantumCircuit,
+                    QuantumRegister)
+from qiskit.tools.visualization import circuit_drawer
 
-# --------------------------------------------------------
-# register so that we can use backends
-# --------------------------------------------------------
+# =============================================================================
+# quantum and classical registers and quantum circuit
+# =============================================================================
 
-APItoken = '3ce852634bcc0d3fc6a5af0920aff9ad4be74ec6972f2a8e06b384796d4b28fd933c7386d2e1296ee5b0425afb317f75d44f558ba6ba18cd8fc899a1fe9fcbb8'
-url = 'https://quantumexperience.ng.bluemix.net/api'
+qreg = QuantumRegister(1)
+creg = ClassicalRegister(1)
+qcircuit = QuantumCircuit(qreg, creg)
 
-register(APItoken, url)
-
-# --------------------------------------------------------
-# make a program, bit registers, and circuit
-# --------------------------------------------------------
-
-qprog = QuantumProgram()
-qbits = qprog.create_quantum_register("qbits", 2)
-cbits = qprog.create_classical_register("cbits", 1)
-
-qcirc = qprog.create_circuit("random", [qbits], [cbits])
-
-# --------------------------------------------------------
+# =============================================================================
 # do the circuit
-# --------------------------------------------------------
+# =============================================================================
 
-qcirc.h(qbits[0])
-qcirc.measure(qbits[0], cbits[0])
+# throw in some gates
+qcircuit.h(qreg[0])
+qcircuit.measure(qreg[0], creg[0])
 
-# --------------------------------------------------------
-# run the job and print the results
-# --------------------------------------------------------
+# run the circuit
+backend = Aer.backends("qasm_simulator")[0]
+result = execute(qcircuit, backend).result()
+counts = result.get_counts()
 
-job = qprog.execute(["random"], 'ibmqx4')
-result = job.result()
+print(counts)
 
-print(result, result.get_counts("random"))
-#print(qcirc.qasm())
+# draw the circuit
+circuit_drawer(qcircuit)
