c760ad6544cb545423b735c9eededd1bc0cb3aee
Ryan LaRose <rlarose@umich.edu>
2018-12-16 14:41:22 -0500

    Updated QISKit compile.py for QISKit v0.6.1.

--- qiskit/compile/compile.py
+++ qiskit/compile/compile.py
@@ -1,35 +1,45 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+"""Example of compiling an algorithm in QISKit v0.6.1."""
 
-# ========================================================
-# compile.py
-#
-# Test the QISKit compiler.
-#
-# written by Ryan LaRose <laroser1@msu.edu>
-# at Michigan State University 05-14-18
-# ========================================================
+# =============================================================================
+# imports
+# =============================================================================
 
-from qiskit import QuantumProgram, available_backends, register, compile
+from qiskit import (IBMQ, compile, ClassicalRegister, execute,
+                    QuantumCircuit, QuantumRegister, register)
 
-# --------------------------------------------------------
+# =============================================================================
 # register so that we can use backends
-# --------------------------------------------------------
+# =============================================================================
 
-qprog= QuantumProgram()
-qbits = qprog.create_quantum_register("qbits", 3)
-cbits = qprog.create_classical_register("cbits", 3)
-qcirc = qprog.create_circuit("random", [qbits], [cbits])
+api = ""
+url = ""
 
-# TODO: unitary you want ot compile goes here
-qcirc.h(qbits[0])
-qcirc.h(qbits[1])
-qcirc.h(qbits[2])
-qcirc.cx(qbits[0],qbits[1])
-qcirc.cx(qbits[0],qbits[2])
+# =============================================================================
+# get a quantum circuit
+# =============================================================================
 
-job = compile(qcirc, 'ibmqx5')
-compiled_circ = job['circuits'][0]['compiled_circuit']['operations']
+# make a circuit
+qreg = QuantumRegister(3)
+circ = QuantumCircuit(qreg)
 
-for i in range(len(compiled_circ)):
-    print(compiled_circ[i])
+# add some instructions
+circ.h(qreg[0])
+circ.h(qreg[1])
+circ.h(qreg[2])
+circ.cx(qreg[0], qreg[1])
+circ.cx(qreg[0], qreg[2])
+
+# =============================================================================
+# pick a backend and compile to it
+# =============================================================================
+
+# get a backend
+backend = IBMQ.backends()[0]
+
+job = compile(circ, backend)
+
+# =============================================================================
+# grab the desired info
+# =============================================================================
+
+print(job.as_dict())
