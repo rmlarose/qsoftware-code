4ef982e6c74f539c58a732cfabbcc11c40d303e6
Ryan LaRose <rlarose@umich.edu>
2018-12-16 14:25:23 -0500

    Updated QISKit simulator_test.py for QISKit v0.6.1.

--- qiskit/simulator-test/simulator-test.py
+++ qiskit/simulator-test/simulator-test.py
@@ -1,77 +1,76 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Mon May 21 20:08:26 2018
+"""Test the QISKit v0.6.1  simulators."""
 
-@author: ryan
-"""
-
-# ============================================
-# simulator-test.py
-#
-# Testing the simulator in QISKit.
-#
-# written by Ryan LaRose <laroser1@msu.edu>
-# at Michigan State University 05-21-18
-# ============================================
-
-# --------------------------------------------
+# =============================================================================
 # imports
-# --------------------------------------------
+# =============================================================================
 
-from qiskit import QuantumProgram, available_backends, register
+from qiskit import (Aer, ClassicalRegister, execute,
+                    QuantumCircuit, QuantumRegister)
 import sys
 import time
 
-# ----------------------------------------------------------
+# =============================================================================
 # number of qubits, depth, and backend to use
-# ----------------------------------------------------------
+# =============================================================================
 
+# number of qubits in circuit
 if len(sys.argv) > 1:
     n = int(sys.argv[1])
 else:
     n = 16
 
+# depth of test circuit
 if len(sys.argv) > 2:
     depth = int(sys.argv[2])
 else:
     depth = 10 
 
+# key for which backend to use
+# 0 == qasm_simulator, 1 == qasm_simulator_py, 2 == statevector_simulator,
+# 3 == statevector_simulator_py, 4 == unitary_simulator, 5 == clifford_sim
 if len(sys.argv) > 3:
     backend_key = int(sys.argv[3])
 else:
-    backend_key = 2
-    
-qprog= QuantumProgram()
-qbits = qprog.create_quantum_register("qbits", n)
-cbits = qprog.create_classical_register("cbits", n)
-qcirc = qprog.create_circuit("test", [qbits], [cbits])
-    
-# timing -- get the start time
-start = time.time()
+    backend_key = 0
+
+# =============================================================================
+# create a circuit, get the simulator
+# =============================================================================
+
+qreg = QuantumRegister(n)
+creg = ClassicalRegister(n)
+qcirc = QuantumCircuit(qreg, creg)
+
+# =============================================================================
+# add the cicuit operations
+# =============================================================================
 
 # main (arbitrary) circuit
 for level in range(depth):
-    for ii in range(len(qbits)):
-        q = qbits[ii]
+    for ii in range(len(qreg)):
+        q = qreg[ii]
         qcirc.h(q)
         qcirc.x(q)
-        if q != qbits[0]:
-            qcirc.cx(q, qbits[0])
+        if q != qreg[0]:
+            qcirc.cx(q, qreg[0])
 
 # measure
-for ii in range(len(qbits)):
-    qcirc.measure(qbits[ii], cbits[ii]) if backend_key != 2 else qcirc.h(qbits[ii])
+for ii in range(len(qreg)):
+    qcirc.measure(qreg[ii], creg[ii])
+
+# =============================================================================
+# get the backend
+# =============================================================================
     
-backends = available_backends()
-backend_used = backends[backend_key]
+backends = Aer.backends()
+backend = backends[backend_key]
 
-# timing -- get the start time
+# =============================================================================
+# execute the circuit and time it
+# =============================================================================
+
 start = time.time()
-
-result = qprog.execute("test", backend='local_statevector_simulator', timeout=1000)
-
-# timing -- get the end time
+result = execute(qcirc, backend)
 runtime = time.time() - start
 
 # print out the runtime
